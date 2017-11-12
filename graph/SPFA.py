588d589090ebb5feb4df76f61d8589847446ccf9
Erick Lv <Erick_Lv@outlook.com>
2017-11-12 11:16:41 +0800

    add SPFA but don not test whehter it is right

---
+++ graph/SPFA.py
@@ -0,0 +1,44 @@
+def SPFA(Graph, start=0):
+    num_node = Graph.get_node_num()
+    graph = Graph.get_graph_list()
+    INF = 1000000
+    dist = [INF] * num_node  # store the minimize distance to start
+    path = [-1] * num_node  # store the previous node in path
+    inq = [0] * num_node  # sign of whether node is in queue
+    for edge in graph(start):
+        dist[edge[0]] = edge[1]
+        path[edge[0]] = start
+    dist[start] = 0
+    path[start] = start
+    inq[start] += 1
+    Q = [start]
+    while len(Q) > 0:
+        u = Q.pop(0)
+        inq[u] -= 1
+        for edge in graph[u]:
+            if dist[u] + edge[1] < dist[edge[0]]:
+                dist[edge[0]] = dist[u] + edge[1]
+                path[edge[0]] = u
+                if inq[edge[0]] == 0:
+                    inq[edge[0]] += 1
+                    Q.append(edge[0])
+    return path
+
+
+# output all path
+def print_path(Graph, start=0):
+    path = SPFA(Graph, start)
+    num_node = Graph.get_node_num()
+    for i in range(num_node):
+        if i == start:
+            continue
+        k = 0
+        shortest = [start] * num_node
+        shortest[k] = i
+        while path[shortest[k]] != start:
+            shortest[k + 1] = path[shortest[k]]
+            k += 1
+        shortest[k + 1] = start
+        for j in range(k - 1, 0, -1):
+            print(shortest[j], end=' ')
+        print(shortest[0])
